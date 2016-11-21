b02a3bbc8bf012dd1ee1fe9c20074dea2233bdde
Paul Colomiets <paul@colomiets.name>
2016-11-22 00:13:50 +0200

    Able to generate some vagga config pieces

--- barnard/barnard/check.py
+++ barnard/barnard/check.py
@@ -4,9 +4,8 @@ import difflib
 import pathlib
 
 import click
-from vagga2lithos.vagga import Config as Vagga
 from vagga2lithos.update import updated_config
-from vagga2lithos import gen, metadata, lithos
+from vagga2lithos import gen, metadata, lithos, vagga
 
 from .main import main as cli
 from .util import write_file
@@ -23,19 +22,25 @@ def check(update, verbose):
     if not CONFIG_DIR.exists():
         CONFIG_DIR.mkdir()
     # TODO(tailhook) check if barnard is itself well-configured
-    vagga = Vagga.load('vagga.yaml')
-    cmd = vagga.commands.get('_deploy-run', vagga.commands.get('run'))
+    config = vagga.Config.load('vagga.yaml')
+    cmd = config.commands.get('_deploy-run', config.commands.get('run'))
+    check_lithos_configs(config, cmd, update, verbose)
+    containers = check_containers(config, cmd, update, verbose)
+    check_barnard(config, containers, update, verbose)
+
+
+def check_lithos_configs(config, cmd, update, verbose):
     lithos_file = CONFIG_DIR / "lithos.yaml"
     if not lithos_file.exists():
-        data = gen.generate_command(vagga, cmd)
+        data = gen.generate_command(config, cmd)
         write_file(lithos_file, data)
     else:
         old_config = lithos.read(lithos_file)
         new_header, new_config = updated_config(old_config, lithos_file,
-            vagga, cmd, verbose=verbose)
+            config, cmd, verbose=verbose)
         if new_config != old_config:
             if update:
-                print("Update to the file:", lithos_file)
+                print("Updates of file:", lithos_file)
             else:
                 print("Proposed changes to the file:", lithos_file)
             old = lithos.dump(old_config)
@@ -46,4 +51,68 @@ def check(update, verbose):
                 d = metadata.dump_header(new_header) + lithos.dump(new_config)
                 write_file(lithos_file, d)
             else:
-                sys.exit(1)
+                # TODO(tailhook) set error code and continue
+                sys.exit(7)
+
+
+def check_containers(config, cmd, update, verbose):
+    cname = '_deploy-' + cmd.container
+    container = config.containers[cname]
+    assert isinstance(container, vagga.Include)
+    filename = container.file
+    old_container = vagga.load_partial(filename)
+    new_container = {
+        'setup': [
+            vagga.Container(cmd.container),
+            vagga.EnsureDir('/app'),
+        ],
+    }
+    dirs = set()
+    for f in getattr(cmd, '_mglawica', {}).get('files', []):
+        if '/' in f:
+            dir = f[:f.index('/')]
+            if dir not in dirs:
+                dirs.add(dir)
+                new_container['setup'].append(vagga.EnsureDir('/app/' + dir))
+        new_container['setup'].append(vagga.Copy(
+            source="/work/" + f,
+            path="/app/" + f,
+        ))
+    if old_container != new_container:
+        if verbose:
+            if update:
+                print("Updating", filename)
+            else:
+                print("Should update", filename)
+        if update:
+            data = vagga.dump(new_container)
+            write_file(filename, data)
+        else:
+            # TODO(tailhook) set error code and continue
+            sys.exit(7)
+    return [cname]
+
+
+def check_barnard(config, containers, update, verbose):
+    container = config.containers['barnard']
+    assert isinstance(container, vagga.Include)
+    filename = container.file
+    old_container = vagga.load_partial(filename)
+    new_container = {
+        'setup': [
+            vagga.Unpack([vagga.Include('barnard.yaml')]),
+        ],
+        'volumes': {},
+        'auto-clean': True,
+    }
+    for cname in containers:
+        dir = '/target/' + cname
+        new_container['setup'].append(vagga.EnsureDir(dir))
+        new_container['volumes'][dir] = vagga.Container(cname)
+        data = vagga.dump(new_container)
+    if update:
+        data = vagga.dump(new_container)
+        write_file(filename, data)
+    else:
+        # TODO(tailhook) set error code and continue
+        sys.exit(7)
