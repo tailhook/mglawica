fef745caebc6b0286ec601625d59901dae360c2a
Paul Colomiets <paul@colomiets.name>
2016-11-21 22:08:15 +0200

    Implement configuration update

--- barnard/barnard/check.py
+++ barnard/barnard/check.py
@@ -1,7 +1,12 @@
 import os
+import sys
+import difflib
 import pathlib
+
+import click
 from vagga2lithos.vagga import Config as Vagga
-from vagga2lithos import gen
+from vagga2lithos.update import updated_config
+from vagga2lithos import gen, metadata, lithos
 
 from .main import main as cli
 from .util import write_file
@@ -10,17 +15,35 @@ CONFIG_DIR = pathlib.Path('/work/barnard')
 
 
 @cli.command()
-def check():
+@click.option('-u', '--update/--no-update', default=False,
+    help='Update files')
+@click.option('-v', '--verbose/--quiet', default=False,
+    help='Print diagnostic messsages')
+def check(update, verbose):
     if not CONFIG_DIR.exists():
         CONFIG_DIR.mkdir()
     # TODO(tailhook) check if barnard is itself well-configured
     vagga = Vagga.load('vagga.yaml')
     cmd = vagga.commands.get('_deploy-run', vagga.commands.get('run'))
-    lithos = CONFIG_DIR / "lithos.yaml"
-    if not lithos.exists():
+    lithos_file = CONFIG_DIR / "lithos.yaml"
+    if not lithos_file.exists():
         data = gen.generate_command(vagga, cmd)
-        write_file(lithos, data)
+        write_file(lithos_file, data)
     else:
-        from vagga2lithos import update
-        gen.check(['vagga.yaml', 'run', CONFIG_DIR / "lithos.yaml"],
-            verbose=True)
+        old_config = lithos.read(lithos_file)
+        new_header, new_config = updated_config(old_config, lithos_file,
+            vagga, cmd, verbose=verbose)
+        if new_config != old_config:
+            if update:
+                print("Update to the file:", lithos_file)
+            else:
+                print("Proposed changes to the file:", lithos_file)
+            old = lithos.dump(old_config)
+            new = lithos.dump(new_config)
+            print('\n'.join(difflib.ndiff(old.splitlines(),
+                                          new.splitlines())))
+            if update:
+                d = metadata.dump_header(new_header) + lithos.dump(new_config)
+                write_file(lithos_file, d)
+            else:
+                sys.exit(1)
