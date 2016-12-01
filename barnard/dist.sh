0f21691f5230660a17372c09730c94121d263971
Paul Colomiets <paul@colomiets.name>
2016-12-01 02:19:14 +0200

    Another fix of distribution script

--- barnard/dist.sh
+++ barnard/dist.sh
@@ -3,7 +3,7 @@
 version=$(git describe)
 mkdir -p dist
 $VAGGA _pack_image -J package > dist/barnard-${version}.tar.xz
-url="http://sh.mglawica.org/barnard-images/barnard-${version}.tar.xz"
+url="http://repo.mglawica.org/barnard-images/barnard-${version}.tar.xz"
 sum=$(sha256sum dist/barnard-${version}.tar.xz | cut -d' ' -f1)
 sed '/url:/{s@http:.*@'$url'@};/sha256:/{s@\w.*@sha256: '$sum'@}' \
     bootstrap.sh  > dist/barnard-testing.sh
